2e85965793da67209ea04578a71eeca2408d195b
dhruv-rajput <dhruvguzi@gmail.com>
2022-02-22 10:01:19 +0530

    Backend

---
+++ SpringBackend/TravelEasy/src/main/java/com/example/TravelEasy/TravelEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravelEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelEasyApplication.class, args);
+	}
+
+}
