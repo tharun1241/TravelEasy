538c9a168289a50e7cdfa95a90e48d03f53e0eb6
tharun1241 <dondeti.tharun@peoplestrong.com>
2022-02-23 09:56:47 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
 import { PaymentComponent } from './confirmation/payment/payment.component';
 import { TravellersComponent } from './confirmation/travellers/travellers.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/signUp/signUp.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { TravellersComponent } from './confirmation/travellers/travellers.compon
     HeaderComponent,
     ConfirmationComponent,
     PaymentComponent,
-    TravellersComponent
+    TravellersComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/auth/signUp/signUp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-signUp',
+  templateUrl: './signUp.component.html',
+  styleUrls: ['./signUp.component.css']
+})
+export class SignUpComponent {
+  title = 'TravelEasy';
+}
