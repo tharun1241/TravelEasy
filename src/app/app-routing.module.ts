54e83e0cb0dfcf0df518b60974d3f594f21ed400
dhruv-rajput <dhruvguzi@gmail.com>
2022-02-23 16:55:08 +0530

    Routing mod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchBusesComponent } from './search-buses/search-buses.component';
 
 const routes: Routes = [
-  {path : 'search', component : SearchBusesComponent},
+  {
+    path : '', component : SearchBusesComponent},
+    {
+    path : 'confirmation' , component : ConfirmationComponent }
 ];
 
 @NgModule({
